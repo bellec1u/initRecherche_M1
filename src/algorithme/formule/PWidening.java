9fa35db0465c88313cca3314210b15ad843367b0
p1ma <pierre.junges@orange.fr>
2017-02-15 19:23:22 +0100

    [ADD] PW selection

--- src/algorithme/formule/PWidening.java
+++ src/algorithme/formule/PWidening.java
@@ -8,23 +8,24 @@ import arbre.Noeud;
 /**
  * @author JUNGES Pierre-Marie - M1 Informatique 2016/2017
  *
- * Jan 26, 2017
+ * Feb 15, 2017
  */
-public class Uct implements FormuleSelection{
+public class PWidening implements FormuleSelection{
 
-	private final double C = Math.sqrt(2.0);
+	private final double C = 2.44;
+	private final double kUCB = Math.sqrt(2.0);
+	private double alpha = 0.666;
 	
 	public Noeud selectionner(Noeud noeud) {
+		int k = (int)(C*Math.pow(noeud.retournerNbSimulation(), alpha));	
+		Noeud enfant = null;
+		int best = 0;
 		double min = Double.NEGATIVE_INFINITY;
 		double bValeur = 0.0;
-		
-		int best = 0;
-		Noeud enfant = null;
-		
-		for( int i = 0 ; i < noeud.retournerNbEnfant() ; i++ ) {
+		for(int i = 0 ; i < k ; i++) {
 			enfant = noeud.retournerEnfant(i);
 			bValeur = ( enfant.rapportVictoireSimulation() ) * Math.pow(-1.0, enfant.getMinOrMax() );
-			bValeur += C * Math.sqrt( Math.log( noeud.retournerNbSimulation() ) / enfant.retournerNbSimulation());
+			bValeur += kUCB * Math.sqrt( Math.log( noeud.retournerNbSimulation() ) / enfant.retournerNbSimulation());
 			if ( bValeur > min ) {
 				min = bValeur;
 				best = i;
